6689c7f0a9d0302a595592a93335fa508390a840
Mark <github@innovatieman.nl>
2025-10-08 23:12:20 +0200

    excl VAT changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   useEmulators: false,
   log_on:false,
   verificationUrl:'http://conversation.alicialabs.com/verify',
-  version: '1.4.21',
+  version: '1.4.22',
   firebase :{
     apiKey: "AIzaSyDc7k8O277P-27n7igbMnYtu8iiJUEk0TE",
     authDomain: "lwai-3bac8.firebaseapp.com",
